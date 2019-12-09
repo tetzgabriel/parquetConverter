05cf5a2a16688222f359e4bcedff4143bcf55888
tetzgabriel <tetz.gabriel@gmail.com>
2019-12-09 16:15:01 -0300

    Fix | parquet parts names

--- parquetConverter.py
+++ parquetConverter.py
@@ -1,20 +1,19 @@
 # encoding=utf8
+import pandas as pd
+import pyarrow.parquet as pq
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-import pandas as pd
-import pyarrow.parquet as pq
-
 print('Appending files')
-files = ['part-00001-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00002-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00003-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00004-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00005-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00006-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00007-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00008-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00009-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00010-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00011-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00012-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00013-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet','part-00014-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet',
-         'part-00015-d97e9529-a3c6-4c51-a47c-74c5a34f3abe-c000.snappy.parquet']
+files = ['part-00001.parquet','part-00002.parquet',
+         'part-00003.parquet','part-00004.parquet',
+         'part-00005.parquet','part-00006.parquet',
+         'part-00007.parquet','part-00008.parquet',
+         'part-00009.parquet','part-00010.parquet',
+         'part-00011.parquet','part-00012.parquet',
+         'part-00013.parquet','part-00014.parquet',
+         'part-00015.parquet']
 
 for f in files:
     print 'Reading ',f
